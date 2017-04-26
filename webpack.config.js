7eef1c0e86b0130c82b652a74d56042597518cb0
zhufuge <344655454@qq.com>
2017-04-26 17:17:17 +0800

    hello-react

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
